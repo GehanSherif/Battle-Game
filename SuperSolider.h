7af560c561f2d120b8bd66c7dd7a5f7509fdf103
Saleh <mogazysaleh@gmail.com>
2021-06-15 01:32:12 +0200

    SS

---
+++ SuperSolider.h
@@ -0,0 +1,17 @@
+#pragma once
+class SuperSolider
+{
+private:
+	int Distance, Destiny;
+	const int speed = 3;
+
+public:
+	SuperSolider(int distance, int destiny);
+
+	//getters
+
+	//setters
+	
+	//operations
+
+};
